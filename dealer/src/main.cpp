04d6d804ec14e628e5e6d59c5c9149faec041e83
xutao <xutao@gmail.com>
2024-09-30 10:30:57 +0700

    init

--- dealer/src/main.cpp
+++ dealer/src/main.cpp
@@ -13,17 +13,16 @@ public:
         // Shell::Initialize();
         // Rml::Initialise();
         // Shell::LoadFonts();
-
         // auto * logWindow = new wxLogWindow(nullptr, "Log Window", true, false);
         // logWindow->GetFrame()->SetSize(500,400);
         // logWindow->GetFrame()->SetPosition(wxPoint(10,10));
 
         other();
 
-        // auto *xnpFrame = new XnpFrame();
-        // xnpFrame->SetSize(500, 320);
-        // xnpFrame->Center();
-        // xnpFrame->Show();
+        auto *xnpFrame = new XnpFrame();
+        xnpFrame->SetSize(500, 320);
+        xnpFrame->Center();
+        xnpFrame->Show();
         return true;
     }
 
