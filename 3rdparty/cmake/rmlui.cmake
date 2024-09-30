5756c7aae89b18a685bc2fd84789d95783a68cba
xutao <xutao@gmail.com>
2024-09-30 10:19:41 +0700

    init

--- 3rdparty/cmake/rmlui.cmake
+++ 3rdparty/cmake/rmlui.cmake
@@ -1,12 +1,23 @@
-# 检查wxWidgets-3.2.6是否已经安装
-if (EXISTS ${CMAKE_INSTALL_PREFIX}/lib/gcc_x64_lib/libwxbase32ud.a OR EXISTS "${CMAKE_INSTALL_PREFIX}/lib/gcc_x64_lib/libwxbase32u.a")
-    message(STATUS "******************** wxWidgets is already installed ********************")
-    set(wxWidgets_LIB_DIR "${CMAKE_INSTALL_PREFIX}/lib/gcc_x64_lib")
-    find_package(wxWidgets REQUIRED)
+file(COPY "${CMAKE_SOURCE_DIR}/3rdparty/RmlUi-6.0/Samples/assets" DESTINATION "${CMAKE_BINARY_DIR}")
+file(COPY "${CMAKE_SOURCE_DIR}/3rdparty/RmlUi-6.0/Samples/basic" DESTINATION "${CMAKE_BINARY_DIR}")
+file(COPY "${CMAKE_SOURCE_DIR}/3rdparty/RmlUi-6.0/Samples/lua_invaders" DESTINATION "${CMAKE_BINARY_DIR}")
+file(COPY "${CMAKE_SOURCE_DIR}/3rdparty/binary-library/win64/lua/" DESTINATION "${CMAKE_BINARY_DIR}")
+file(COPY "${CMAKE_SOURCE_DIR}/3rdparty/binary-library/win64/runtime/" DESTINATION "${CMAKE_BINARY_DIR}")
+file(GLOB FREETYPE_DLL_FILES "${CMAKE_SOURCE_DIR}/3rdparty/binary-library/win64/freetype/*.dll")
+foreach(FREETYPE_DLL_FILE ${FREETYPE_DLL_FILES})
+    file(COPY "${FREETYPE_DLL_FILE}" DESTINATION "${CMAKE_BINARY_DIR}")
+endforeach()
+# 检查RmlUI是否已经安装
+if (EXISTS ${CMAKE_INSTALL_PREFIX}/lib/librmlui.dll.a)
+    file(COPY "${CMAKE_INSTALL_PREFIX}/bin/" DESTINATION "${CMAKE_BINARY_DIR}")
+    message(STATUS "******************** RmlUi is already installed ********************")
+
 else ()
-    #set(wxBUILD_SAMPLES All)
-    set(wxBUILD_SHARED OFF)
-    set(wxBUILD_MONOLITHIC OFF)
-    set(wxBUILD_USE_STATIC_RUNTIME ON)
-    add_subdirectory(wxWidgets-3.2.6)
+    set(BUILD_SHARED_LIBS ON)           # RMLUI动态库
+    set(RMLUI_SVG_PLUGIN OFF)           # RMLUI插件SVG
+    set(RMLUI_LUA_BINDINGS ON)          # RMLUI插件Lua
+    set(RMLUI_SAMPLES OFF)              # RMLUI库示例程序生成
+    set(RMLUI_BACKEND GLFW_GL3)         # RMLUI库绘制引擎
+    set(RMLUI_IS_ROOT_PROJECT TRUE)     # RMLUI库绘制引擎
+    add_subdirectory(RmlUi-6.0)         # RMLUI库生成
 endif ()
