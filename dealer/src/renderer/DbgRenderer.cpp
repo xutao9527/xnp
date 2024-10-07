2be1b11b82a359cd7c4df1916ecd9e4ccac41647
xutao <xutao@gmail.com>
2024-10-07 15:59:50 +0700

    init

--- dealer/src/renderer/DbgRenderer.cpp
+++ dealer/src/renderer/DbgRenderer.cpp
@@ -9,7 +9,7 @@ void DbgRenderer::Init()
     key_down_callback = &Shell::ProcessKeyDownShortcuts;
     context = Rml::CreateContext(Shell::ConvertToString(window_title), Rml::Vector2i(window_width, window_height),&render_interface);
     Rml::ElementDocument *document = context->LoadDocument("assets/demo.rml");
-    document->Show();
     Rml::Debugger::Initialise(context);
+    document->Show();
 }
 
