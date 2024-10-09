d84d89c6579c06918fa4d8e2aef5b4cf11f13efc
xutao <xutao@gmail.com>
2024-10-09 16:40:43 +0700

    11

--- dealer/src/ui/XnpFrame.cpp
+++ dealer/src/ui/XnpFrame.cpp
@@ -1,3 +1,5 @@
+#include <wx/dcbuffer.h>
+#include <wx/graphics.h>
 #include "XnpFrame.h"
 
 // IMPLEMENT_DYNAMIC_CLASS(XnpFrame, wxFrame)
@@ -6,3 +8,11 @@ wxBEGIN_EVENT_TABLE(XnpFrame, wxFrame)
                 EVT_PAINT(XnpFrame::OnPaint)
 wxEND_EVENT_TABLE()
 
+void XnpFrame::OnPaint(wxPaintEvent& e){
+
+    //wxBufferedPaintDC dc(this);
+    //if(image.IsOk()){
+    //    dc.DrawBitmap(image.Scale(image.GetWidth(),image.GetHeight()),0,0,true);
+    //}
+
+}
--- dealer/src/ui/XnpFrame.h
+++ dealer/src/ui/XnpFrame.h
@@ -8,6 +8,7 @@
 
 #endif
 
+#include <wx/graphics.h>
 #include <string>
 #include <locale>
 #include <codecvt>
@@ -18,26 +19,58 @@
 class XnpFrame : public wxFrame
 {
     std::weak_ptr<DbgRenderer> rendererContext;
-    wxTextCtrl* textCtrl;  // 文本输入框
+    wxImage image;
 public:
-    XnpFrame(wxWindow *parent,wxWindowID id,const wxString& title) : wxFrame(parent, id, title)
+    XnpFrame(wxWindow *parent,wxWindowID id,const wxString& title)
+    : wxFrame(parent, id, title,wxDefaultPosition,wxSize(512, 384))
     {
         SetIcon(wxICON(IDI_DEALER_ICON));
-        std::shared_ptr<DbgRenderer> context = std::make_shared<DbgRenderer>(GetHWND(),
-                                                                                         GetTitle().ToStdWstring(),
-                                                                                         GetClientRect().GetWidth(),
-                                                                                         GetClientRect().GetHeight());
-        rendererContext = std::weak_ptr<DbgRenderer>(context);
-        context->Run();
-        context.reset();
+        // std::shared_ptr<DbgRenderer> context = std::make_shared<DbgRenderer>(GetHWND(),
+        //                                                                                  GetTitle().ToStdWstring(),
+        //                                                                                  GetClientRect().GetWidth(),
+        //                                                                                  GetClientRect().GetHeight());
+        // rendererContext = std::weak_ptr<DbgRenderer>(context);
+        // context->Run();
+        // context.reset();
+        // 设置背景颜色为透明
+        //SetBackgroundStyle(wxBG_STYLE_PAINT);  // 设置背景样式
+        //SetTransparent(150);  // 设置窗体半透明
 
-        // 创建一个文本输入框
-        textCtrl = new wxTextCtrl(this, wxID_ANY, wxT(""), wxDefaultPosition);
 
-        // 设置布局
-        wxBoxSizer* sizer = new wxBoxSizer(wxVERTICAL);
-        sizer->Add(textCtrl, 0, wxALL | wxEXPAND, 10);
-        SetSizer(sizer);
+        // // 创建 wxBitmap 对象
+        // wxBitmap bitmap(200, 200);
+        // // 创建 wxMemoryDC 对象
+        // wxMemoryDC memDC(bitmap);
+        // // 使用 wxGraphicsContext 来绘制抗锯齿的图形
+        // wxGraphicsContext* gc = wxGraphicsContext::Create(memDC);
+        // if (gc) {
+        //     // 设置没有边框的画笔 (wxTRANSPARENT_PEN)
+        //     gc->SetPen(*wxTRANSPARENT_PEN);
+        //
+        //     // 设置填充颜色为红色
+        //     gc->SetBrush(*wxRED_BRUSH);
+        //
+        //     // 绘制一个带抗锯齿效果的圆角矩形
+        //     //gc->DrawRoundedRectangle(0, 0, 200, 200, 50);
+        //     int radius = 100; // 圆半径
+        //     gc->DrawEllipse(0, 0, radius * 2, radius * 2); // 绘制圆形
+        //
+        //     // 删除 gc 对象
+        //     delete gc;
+        // }
+        //
+        // // 完成绘制后，解除 wxMemoryDC 和 wxBitmap 的关联
+        // memDC.SelectObject(wxNullBitmap);
+        //
+        // wxImage image = bitmap.ConvertToImage();
+        // image.InitStandardHandlers();
+        // // 保存为 PNG 文件
+        // if (image.SaveFile("circle.png")) {
+        //     wxLogMessage("Image saved successfully as " );
+        // }
+        //
+
+        // dc.DrawBitmap(bitmap, 0, 0, false);  // 将 bitmap 绘制到窗口上
     }
 
     WXLRESULT MSWWindowProc(WXUINT message, WXWPARAM wParam, WXLPARAM lParam) override
@@ -56,6 +89,12 @@ public:
         }
         return wxFrame::MSWWindowProc(message, wParam, lParam);
     }
+
+    void SetImage(const wxImage& skin){
+        image = skin;
+    }
+
+    void OnPaint(wxPaintEvent& e);
 private:
 wxDECLARE_EVENT_TABLE();
 };
