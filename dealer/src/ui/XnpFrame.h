1b2e75f58c17435369d6be87936e31bb28347def
xutao <xutao@gmail.com>
2024-10-10 20:45:26 +0700

    较为完美的一版

--- dealer/src/ui/XnpFrame.h
+++ dealer/src/ui/XnpFrame.h
@@ -23,75 +23,68 @@ class XnpFrame : public wxFrame
     wxImage shapeImage;
 public:
     XnpFrame(wxWindow *parent,
-    wxWindowID id,
-    const wxString& title,
-    const wxPoint& pos = wxDefaultPosition,
-    const wxSize& size = wxDefaultSize,
-    long style = wxDEFAULT_FRAME_STYLE,
-    const wxString& name = wxASCII_STR(wxFrameNameStr))
-    : wxFrame(parent, id, title,pos,size, style,name)
+             wxWindowID id,
+             const wxString &title,
+             const wxPoint &pos = wxDefaultPosition,
+             const wxSize &size = wxDefaultSize,
+             long style = wxDEFAULT_FRAME_STYLE,
+             const wxString &name = wxASCII_STR(wxFrameNameStr))
+            : wxFrame(parent, id, title, pos, size, style, name)
     {
         SetIcon(wxICON(IDI_DEALER_ICON));
-        //SetWindowStyle(wxNO_BORDER);
-        std::shared_ptr<DbgRenderer> context = std::make_shared<DbgRenderer>(GetHWND(),
-                                                                                            GetTitle().ToStdWstring(),
-                                                                                            GetClientRect().GetWidth(),
-                                                                                            GetClientRect().GetHeight()
-                                                                            );
-        std::cout << "XnpFrame" << std::endl;
-        rendererContext = context;
-        context->Run();
-
-        // rendererContext = std::weak_ptr<DbgRenderer>(context);
-        // context->Run();
-        // context.reset();
-        //SetWindowStyle(wxNO_BORDER|wxFRAME_SHAPED);
+        SetWindowStyle(wxNO_BORDER | wxFRAME_SHAPED);
+        rendererContext = std::make_shared<DbgRenderer>(GetHWND(),GetTitle().ToStdWstring(),GetClientRect().GetWidth(),GetClientRect().GetHeight()
+        );
+        rendererContext->Run();
         //InitShapeImage();
     }
 
-    ~XnpFrame() override{
+    ~XnpFrame() override
+    {
         std::cout << "~XnpFrame" << std::endl;
     }
 
-    void InitShapeImage(){
-        wxBitmap bitmap(GetClientRect().GetWidth(), GetClientRect().GetHeight(),32); // 创建支持透明的位图
+    void InitShapeImage()
+    {
+        wxBitmap bitmap(GetClientRect().GetWidth(), GetClientRect().GetHeight(), 32); // 创建支持透明的位图
         bitmap.UseAlpha(true);
         wxMemoryDC memDC(bitmap);
         // 设置背景为透明
         memDC.SetBackground(wxBrush(wxColour(0, 0, 0, 0))); // 设置透明背景
         memDC.Clear(); // 清空位图以应用透明背景
         // 创建图形上下文
-        wxGraphicsContext* gc = wxGraphicsContext::Create(memDC);
+        wxGraphicsContext *gc = wxGraphicsContext::Create(memDC);
         if (gc) {
             gc->SetAntialiasMode(wxANTIALIAS_DEFAULT);
             gc->SetBrush(*wxRED_BRUSH); // 设置填充颜色为红色
             gc->SetPen(wxColour(0, 255, 0)); // 设置透明边框
-            gc->DrawRoundedRectangle(0, 0, GetClientRect().GetWidth()-2, GetClientRect().GetHeight()-2, 8); // 绘制圆角矩形
+            gc->DrawRoundedRectangle(0, 0, GetClientRect().GetWidth() - 2, GetClientRect().GetHeight() - 2,
+                                     8); // 绘制圆角矩形
             delete gc; // 删除图形上下文
         }
         // 完成绘制后，解除 wxMemoryDC 和 wxBitmap 的关联
         memDC.SelectObject(wxNullBitmap);
         shapeImage = bitmap.ConvertToImage();
-        if(shapeImage.IsOk()){
-            if(shapeImage.HasAlpha())
-            {
+        if (shapeImage.IsOk()) {
+            if (shapeImage.HasAlpha()) {
                 shapeImage.ConvertAlphaToMask();
             }
-            SetShape(wxRegion(shapeImage.Scale(GetClientRect().GetWidth(),GetClientRect().GetHeight())));
+            SetShape(wxRegion(shapeImage.Scale(GetClientRect().GetWidth(), GetClientRect().GetHeight())));
         }
     }
 
     WXLRESULT MSWWindowProc(WXUINT message, WXWPARAM wParam, WXLPARAM lParam) override
     {
-        if(auto context = rendererContext){
+        if (auto context = rendererContext) {
             // 文本框输出,激活输入法动态候选框位置
-            if(message == WM_IME_COMPOSITION){
+            if (message == WM_IME_COMPOSITION) {
                 context->ActivateKeyboard();
             }
             // 派发事件到自绘引擎
-            context->DispatchEvent(message,wParam,lParam);
+            context->DispatchEvent(message, wParam, lParam);
             //屏蔽 wxwidgets IME事件
-            if (message == WM_IME_STARTCOMPOSITION || message == WM_IME_ENDCOMPOSITION || message == WM_IME_COMPOSITION || message == WM_IME_CHAR || message == WM_IME_REQUEST){
+            if (message == WM_IME_STARTCOMPOSITION || message == WM_IME_ENDCOMPOSITION ||
+                message == WM_IME_COMPOSITION || message == WM_IME_CHAR || message == WM_IME_REQUEST) {
                 return 0;
             }
         }
