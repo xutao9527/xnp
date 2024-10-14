6a78bb7be56bf37543f9eb803b456bf3c04e958a
xutao <xutao@gmail.com>
2024-10-14 16:25:18 +0700

    e

--- xnp_dealer/src/ui/RmlQWidget.h
+++ xnp_dealer/src/ui/RmlQWidget.h
@@ -26,15 +26,15 @@ protected:
         QWidget::showEvent(event);
         // 获取窗口 HWND (winId() 返回的是 HWND)
         HWND hwnd = reinterpret_cast<HWND>(this->winId());
-        qDebug() << "窗口 HWND:" << hwnd;
+        //qDebug() << "窗口 HWND:" << hwnd;
         // 获取窗口标题
         QString title = this->windowTitle();
-        qDebug() << "窗口标题:" << title;
+        //qDebug() << "窗口标题:" << title;
         // 获取客户区的宽度和高度
         QRect contents = this->contentsRect();
         int clientWidth = contents.width();
         int clientHeight = contents.height();
-        qDebug() << "客户区宽度:" << clientWidth << "客户区高度:" << clientHeight;
+        //qDebug() << "客户区宽度:" << clientWidth << "客户区高度:" << clientHeight;
         rendererContext = std::make_shared<DbgRenderer>(hwnd,title.toStdString(),clientWidth,clientHeight);
         rendererContext->Run();
     }
@@ -46,13 +46,14 @@ protected:
             if (msg->message == WM_IME_COMPOSITION) {
                 context->ActivateKeyboard();
             }
-            // 派发事件到自绘引擎
-            context->DispatchEvent(msg->message, msg->wParam, msg->lParam);
             //屏蔽 wxwidgets IME事件
-            // if (msg->message == WM_IME_STARTCOMPOSITION || msg->message == WM_IME_ENDCOMPOSITION ||
-            //         msg->message == WM_IME_COMPOSITION || msg->message == WM_IME_CHAR || msg->message == WM_IME_REQUEST) {
-            //
-            // }
+            if (msg->message == WM_IME_STARTCOMPOSITION || msg->message == WM_IME_ENDCOMPOSITION ||
+                    msg->message == WM_IME_COMPOSITION || msg->message == WM_IME_CHAR || msg->message == WM_IME_REQUEST) {
+
+            }else{
+                // 派发事件到自绘引擎
+                context->DispatchEvent(msg->message, msg->wParam, msg->lParam);
+            }
         }
         return QWidget::nativeEvent(eventType, message, result);
     }
