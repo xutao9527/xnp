7ee2361e75a96e957c232f20f7e6477e2b857eaf
xutao <xutao@gmail.com>
2024-10-14 10:03:14 +0700

    qt版

--- txh_dealer/src/main.cpp
+++ txh_dealer/src/main.cpp
@@ -15,16 +15,17 @@ int main(int argc, char *argv[]) {
 
     // 创建主窗口
     QWidget window;
-    window.setWindowTitle("Centered Text Example");
+    window.setWindowTitle("Example");
     window.resize(400, 300); // 设置窗口大小
 
-    window.setWindowIcon(QIcon("resources/icon.ico"));
+    QIcon icon("resources/icon.ico");
+    window.setWindowIcon(icon);
 
     // 创建一个 QLabel
-    QLabel *label = new QLabel("Hello, Qt!阿");
+    auto *label = new QLabel("Hello, Qt!爱的色放");
 
     // 创建一个垂直布局并将 QLabel 添加到其中
-    QVBoxLayout *layout = new QVBoxLayout(&window);
+    auto *layout = new QVBoxLayout(&window);
     layout->addWidget(label); // 添加 QLabel 到布局
 
     // 设置布局
