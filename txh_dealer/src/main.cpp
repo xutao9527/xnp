7239f87ccfdbac525356e2818de3640d832377ba
xutao <xutao@gmail.com>
2024-10-14 14:37:42 +0700

    qt版

--- txh_dealer/src/main.cpp
+++ txh_dealer/src/main.cpp
@@ -3,11 +3,10 @@
 #include <QIcon>
 #include "ui/RmlQWidget.h"
 
-
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
-
     RmlQWidget window;
+    window.setWindowTitle("游戏引擎");  // 设置窗口标题
     window.resize(512, 384); // 设置窗口大小
     window.show();
 
--- txh_dealer/src/ui/RmlQWidget.h
+++ txh_dealer/src/ui/RmlQWidget.h
@@ -2,6 +2,9 @@
 
 #include <QWidget>
 #include <QDebug>
+#include <QEvent>
+#include <QMouseEvent>
+#include <windows.h>
 
 class RmlQWidget : public QWidget
 {
@@ -14,13 +17,29 @@ protected:
     void showEvent(QShowEvent *event) override
     {
         QWidget::showEvent(event);
-
-        // 获取窗口的宽度和高度
-        int width = this->width();
-        int height = this->height();
-
-        qDebug() << "窗口宽度:" << width << "高度:" << height;
+        // 获取窗口 HWND (winId() 返回的是 HWND)
+        HWND hwnd = reinterpret_cast<HWND>(this->winId());
+        qDebug() << "窗口 HWND:" << hwnd;
+        // 获取窗口标题
+        QString title = this->windowTitle();
+        qDebug() << "窗口标题:" << title;
+        // 获取客户区的宽度和高度
+        QRect contents = this->contentsRect();
+        int clientWidth = contents.width();
+        int clientHeight = contents.height();
+        qDebug() << "客户区宽度:" << clientWidth << "客户区高度:" << clientHeight;
     }
+
+    bool nativeEvent(const QByteArray &eventType, void *message, long *result) override{
+        MSG *msg = static_cast<MSG *>(message);
+        // 输出原生事件信息
+        // qDebug() << "Received native event:"
+        //          << "Message:" << msg->message
+        //          << "wParam:" << msg->wParam
+        //          << "lParam:" << msg->lParam;
+        return QWidget::nativeEvent(eventType, message, result);
+    }
+
 };
 
 
