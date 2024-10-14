504144c9d4662a8f3de1ccd19ea42281cc3dd9ea
xutao <xutao@gmail.com>
2024-10-14 11:06:12 +0700

    qt版

--- txh_dealer/src/main.cpp
+++ txh_dealer/src/main.cpp
@@ -10,30 +10,28 @@
 int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
 
-    // 打印当前工作目录，调试图标路径问题
-    qDebug() << "Current working directory:" << QDir::currentPath();
-
     // 创建主窗口
     QWidget window;
     window.setWindowTitle("Example");
     window.resize(400, 300); // 设置窗口大小
 
-    QIcon icon("resources/icon.ico");
-    window.setWindowIcon(icon);
+    // 窗口图标
+    QIcon icon(":/resources/icon.ico");
+    if (icon.isNull()) {
+        qDebug() << "Icon is null!";
+    } else {
+        window.setWindowIcon(icon);
+    }
 
-    // 创建一个 QLabel
-    auto *label = new QLabel("Hello, Qt!爱的色放");
+    //窗口图片
+    QPixmap pixmap(":/resources/demo.png");
+    QLabel *iconLabel = new QLabel;
+    iconLabel->setPixmap(pixmap);
+    iconLabel->setAlignment(Qt::AlignCenter); // 设置文本居中对齐
+    QVBoxLayout *layout = new QVBoxLayout(&window);
+    layout->addWidget(iconLabel, 0, Qt::AlignCenter); // 将 QLabel 居中
 
-    // 创建一个垂直布局并将 QLabel 添加到其中
-    auto *layout = new QVBoxLayout(&window);
-    layout->addWidget(label); // 添加 QLabel 到布局
-
-    // 设置布局
-    layout->setAlignment(label, Qt::AlignCenter); // 让 QLabel 在布局中居中
-
-    // 显示窗口
     window.setLayout(layout);
     window.show();
-
     return app.exec();
 }
