dc7b151f018e2bc5e1eb414a1c62a3dd9d962385
xutao <xutao@gmail.com>
2024-10-14 15:33:01 +0700

    qt版

---
+++ xnp_dealer/src/renderer/DbgRenderer.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "XnpWin32VKContext.h"
+#include <iostream>
+
+class DbgRenderer : public XnpWin32VKContext
+{
+
+public:
+    void Init() override;
+    DbgRenderer(HWND win_hwnd, std::string title, int width, int height)
+            : XnpWin32VKContext(win_hwnd, std::move(title), width, height)
+    {
+        std::cout << "DbgRenderer" << std::endl;
+    }
+
+    ~DbgRenderer() override {
+        std::cout << "~DbgRenderer" << std::endl;
+    }
+};
