bbbf20a415b7ef7e5fd271950c31674bf4d36fd3
xutao <xutao@gmail.com>
2024-10-28 11:53:38 +0700

    xnp_view
    Change-Id: Ic6a2579fa21c158c8603e7b3c4683394c1229a41

---
+++ xnp_view/src/main.cpp
@@ -0,0 +1,17 @@
+﻿#include <QApplication>
+#include <QVBoxLayout>
+#include <QIcon>
+#include <QWidget>
+#include <fcntl.h>
+#include <io.h>
+
+
+int main(int argc, char *argv[]) {
+    QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+    QApplication app(argc, argv);
+    QWidget window;
+    window.setWindowTitle(u8"游戏引擎");  // 设置窗口标题
+    window.resize(512, 384); // 设置窗口大小
+    window.show();
+    return app.exec();
+}
